455023bda579408e1d9f9a666b63d2e323030113
Alexey Zavalin <lehazavalin95@gmail.com>
2023-04-11 22:00:52 +1000

    правки

--- script.js
+++ script.js
@@ -4,32 +4,6 @@ const secondNumber = document.getElementById('second_number');
 firstNumber.value = 0;
 secondNumber.value = 0;
 
-function calculate(firstNumber, secondNumber, operator) {
-    let result = '0';
-    if (secondNumber == 0 && operator == '/') {
-        return 'На 0 делить нельзя';
-    }
-    if (firstNumber !== '' && secondNumber !== '') {
-        switch (operator) {
-            case '+':
-                result = firstNumber + secondNumber;
-                break;
-            case '-':
-                result = firstNumber - secondNumber;
-                break;
-            case '*':
-                result = firstNumber * secondNumber;
-                break;
-            case '/':
-                result = firstNumber / secondNumber;
-                break;
-            default:
-                break;
-        }
-    }
-    return result;
-}
-
 const btns = document.querySelectorAll('.calculator__btn');
 const resultContainer = document.querySelector('.calculator__result');
 
